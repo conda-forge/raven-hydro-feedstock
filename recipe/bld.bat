49abdd96d62a393dc23280b14104a0cd72f61fc2
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2022-02-23 14:39:43 -0500

    point to src_dir

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,16 @@
 setlocal EnableDelayedExpansion
 
 :: Make a build folder and change to it.
-mkdir build
-cd build
+mkdir build-raven
+cd build-raven
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
-@REM       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-@REM       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-@REM       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DCMAKE_CXX_FLAGS="%CXXFLAGS% -DNETCDF -WNO-DEPRECATED" ^
-      .. ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DCMAKE_CXX_FLAGS="%CXXFLAGS% -DNETCDF" ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: Build!
