ee068e7aa87f3ce6273745b49afe346c69a5a887
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2022-02-24 13:44:29 -0500

    move cmakelists to src_dir

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,14 @@
 setlocal EnableDelayedExpansion
 
 :: Make a build folder and change to it.
-mkdir build-raven
-move CMakeLists.txt build-raven
-cd build-raven
+move CMakeLists.txt %SRC_DIR%
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DCMAKE_CXX_FLAGS="%CXXFLAGS% -DNETCDF" ^
+      -DCMAKE_CXX_FLAGS="%CXXFLAGS% -Dnetcdf" ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
@@ -21,3 +19,7 @@ if errorlevel 1 exit 1
 :: Install!
 nmake install
 if errorlevel 1 exit 1
+
+@REM :: Move executable to binary folder - If needed
+@REM copy Raven.exe %LIBRARY_BIN%
+@REM if errorlevel 1 exit 1
